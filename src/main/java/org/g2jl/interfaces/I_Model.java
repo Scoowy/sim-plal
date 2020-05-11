60b0370c98eb0ba2654c5eae84e4699ecdcb74ce
Scoowy <gahonajuanjo@gmail.com>
2020-05-11 12:49:23 -0500

    Add interfaces

---
+++ src/main/java/org/g2jl/interfaces/I_Model.java
@@ -0,0 +1,10 @@
+package org.g2jl.interfaces;
+
+/**
+ * Clase Interface <b>I_Model</b>
+ *
+ * @author Juan Gahona
+ * @version 20.5.10
+ */
+public interface I_Model {
+}
---
+++ src/main/java/org/g2jl/interfaces/I_View.java
@@ -0,0 +1,11 @@
+package org.g2jl.interfaces;
+
+/**
+ * Clase Interface <b>I_View</b>
+ *
+ * @author Juan Gahona
+ * @version 20.5.10
+ */
+public interface I_View {
+    public void addButtons();
+}
