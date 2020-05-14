f4d055f72b9ea77148950ac3079a4ce2117b43b5
Scoowy <gahonajuanjo@gmail.com>
2020-05-13 20:20:37 -0500

    Add counter to canvas

---
+++ src/main/java/org/g2jl/models/BurstCounter.java
@@ -0,0 +1,96 @@
+package org.g2jl.models;
+
+import java.awt.*;
+
+
+/**
+ * Clase que pinta un cuadro con el contador de ráfagas de CPU actuales.
+ *
+ * @author Juan Gahona
+ * @version 20.5.13
+ */
+public class BurstCounter {
+    private int x;
+    private int y;
+    private final int w = 70;
+    private final int h = 30;
+
+    private int burst;
+
+    private final Color strokeColor = Color.BLACK;
+    private final Color backgroundColor = Color.WHITE;
+    private final Color textColor = Color.DARK_GRAY;
+
+    private Font fuente;
+    private Dimension pointFont;
+
+    /**
+     * Constructor de clase.
+     *
+     * @param burst rafaga de CPU actual
+     */
+    public BurstCounter(int burst) {
+        this.burst = burst;
+        this.x = 0;
+        this.y = 0;
+
+        this.fuente = null;
+        this.pointFont = new Dimension(x, y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getBurst() {
+        return burst;
+    }
+
+    public void setBurst(int burst) {
+        this.burst = burst;
+    }
+
+    public void calculatePosition(Dimension extremePoint) {
+        this.x = extremePoint.width - this.w;
+        this.y = extremePoint.height - this.h;
+    }
+
+    public String changeFontMetrics(Graphics g) {
+        Font oldFont = g.getFont();
+        this.fuente = new Font(oldFont.getName(), oldFont.getStyle(), 26);
+        FontMetrics fm = g.getFontMetrics(fuente);
+
+        String burstStr = Integer.toString(this.burst);
+        int anchoBurstStr = fm.stringWidth(burstStr);
+        int altoBurstStr = fm.getHeight();
+        pointFont.width = x + ((w - anchoBurstStr) / 2);
+        pointFont.height = y + (((h - altoBurstStr) / 2) + fm.getAscent());
+
+        return burstStr;
+    }
+
+    public void paintCounter(Graphics g) {
+        String texto = changeFontMetrics(g);
+
+        g.setColor(backgroundColor);
+        g.fillRect(x, y, w, h);
+        g.setColor(strokeColor);
+        g.drawRect(x, y, w, h);
+
+        g.setFont(fuente);
+        g.setColor(this.textColor);
+        g.drawString(texto, pointFont.width, pointFont.height);
+    }
+}
--- src/main/java/org/g2jl/models/DiagramaGant.java
+++ src/main/java/org/g2jl/models/DiagramaGant.java
@@ -3,9 +3,7 @@ package org.g2jl.models;
 import org.jdesktop.swingx.JXPanel;
 
 import javax.swing.border.TitledBorder;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,20 +11,22 @@ import java.util.List;
  * Clase heredada de un JXFrame en la que se dibujara el diagrama de Gant.
  *
  * @author Juan Gahona
- * @version 20.5.12
+ * @version 20.5.13
  */
 public class DiagramaGant extends JXPanel {
     private List<CellGant> cells;
     private Dimension dimensionPanel;
+    private BurstCounter counter;
 
     // Padding Left Top Right Bottom
     private final int[] padds = {20, 20, 20, 20};
 
     public DiagramaGant() {
         setBorder(new TitledBorder(null, "Diagrama de Gant", TitledBorder.LEFT, TitledBorder.TOP));
-        setBackground(Color.BLUE);
+//        setBackground(Color.BLUE);
         cells = new ArrayList<CellGant>();
         dimensionPanel = getSize();
+        counter = new BurstCounter(123);
     }
 
     public void processListToCellList(List<Process> process) {
@@ -39,11 +39,19 @@ public class DiagramaGant extends JXPanel {
         dimensionPanel.height = dimensionPanel.height - padds[3] * 2;
     }
 
+    public void recalculateComponents(Graphics g) {
+        recalculateDimension();
+        Dimension panelBRPoint = new Dimension(dimensionPanel.width + padds[0], dimensionPanel.height + padds[1] * 2 - 3);
+        counter.calculatePosition(panelBRPoint);
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        recalculateDimension();
+        recalculateComponents(g);
         String texto = String.format("%d - %d", dimensionPanel.width, dimensionPanel.height);
         g.drawRect(padds[0], padds[1], dimensionPanel.width, dimensionPanel.height);
-        g.drawString(texto, 10, 20);
+
+        g.drawChars(texto.toCharArray(), 0, texto.length(), 10, 20);
+        counter.paintCounter(g);
     }
 }
