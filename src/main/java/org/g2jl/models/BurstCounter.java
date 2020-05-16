a696692315f2e2657a8c1aacbb87d263a1de1c59
Scoowy <gahonajuanjo@gmail.com>
2020-05-15 20:18:42 -0500

    Rewrite for use UtilGraphics class

--- src/main/java/org/g2jl/models/BurstCounter.java
+++ src/main/java/org/g2jl/models/BurstCounter.java
@@ -1,5 +1,7 @@
 package org.g2jl.models;
 
+import org.g2jl.utils.UtilGraphics;
+
 import java.awt.*;
 
 /**
@@ -9,48 +11,18 @@ import java.awt.*;
  * @version 20.5.15
  */
 public class BurstCounter {
-    private int x;
-    private int y;
-    private final int w = 70;
-    private final int h = 30;
-
+    private Dimension topLeft;
+    private final Dimension bottomRight = new Dimension(70, 30);
     private int burst;
-
-    private final Color STROKE_COLOR = Color.BLACK;
-    private final Color BACKGROUND_COLOR = Color.WHITE;
-    private final Color TEXT_COLOR = Color.DARK_GRAY;
-
-    private Font fuente;
-    private Dimension pointFont;
 
     /**
      * Constructor de clase.
      *
-     * @param burst rafaga de CPU actual
+     * @param burst ráfaga de CPU actual
      */
     public BurstCounter(int burst) {
+        this.topLeft = new Dimension(0, 0);
         this.burst = burst;
-        this.x = 0;
-        this.y = 0;
-
-        this.fuente = null;
-        this.pointFont = new Dimension(x, y);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 
     public int getBurst() {
@@ -62,34 +34,19 @@ public class BurstCounter {
     }
 
     public void calculatePosition(Dimension extremePoint) {
-        this.x = extremePoint.width - this.w;
-        this.y = extremePoint.height - this.h;
-    }
-
-    public String changeFontMetrics(Graphics g) {
-        Font oldFont = g.getFont();
-        this.fuente = new Font(oldFont.getName(), oldFont.getStyle(), 26);
-        FontMetrics fm = g.getFontMetrics(fuente);
-
-        String burstStr = Integer.toString(this.burst);
-        int anchoBurstStr = fm.stringWidth(burstStr);
-        int altoBurstStr = fm.getHeight();
-        pointFont.width = x + ((w - anchoBurstStr) / 2);
-        pointFont.height = y + (((h - altoBurstStr) / 2) + fm.getAscent());
-
-        return burstStr;
+        topLeft.width = extremePoint.width - bottomRight.width;
+        topLeft.height = extremePoint.height - bottomRight.height;
     }
 
     public void paintCounter(Graphics g) {
-        String texto = changeFontMetrics(g);
+        g.setColor(UtilGraphics.BACKGROUND_COLOR_COUNTER);
+        g.fillRect(topLeft.width, topLeft.height, bottomRight.width, bottomRight.height);
+        g.setColor(UtilGraphics.STROKE_COLOR);
+        g.drawRect(topLeft.width, topLeft.height, bottomRight.width, bottomRight.height);
 
-        g.setColor(BACKGROUND_COLOR);
-        g.fillRect(x, y, w, h);
-        g.setColor(STROKE_COLOR);
-        g.drawRect(x, y, w, h);
-
-        g.setFont(fuente);
-        g.setColor(this.TEXT_COLOR);
-        g.drawString(texto, pointFont.width, pointFont.height);
+        g.setFont(UtilGraphics.FONT_COUNTER);
+        g.setColor(UtilGraphics.TEXT_COLOR);
+        Dimension posText = UtilGraphics.centerText(g, topLeft, bottomRight, burst);
+        g.drawString(Integer.toString(burst), posText.width, posText.height);
     }
 }
