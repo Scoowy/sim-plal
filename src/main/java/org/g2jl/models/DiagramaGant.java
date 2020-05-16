e19765106dcc6c2f0127b1c28d872dada8f25782
Scoowy <gahonajuanjo@gmail.com>
2020-05-16 01:21:12 -0500

    Prepare DiagramaGant class for run with UtilData and UtilGraph class

--- src/main/java/org/g2jl/models/DiagramaGant.java
+++ src/main/java/org/g2jl/models/DiagramaGant.java
@@ -2,9 +2,11 @@ package org.g2jl.models;
 
 import javafx.scene.control.Cell;
 import org.g2jl.controllers.C_Canvas;
+import org.g2jl.utils.UtilData;
 import org.g2jl.utils.UtilGraphics;
 import org.jdesktop.swingx.JXPanel;
 
+import javax.rmi.CORBA.Util;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.util.ArrayList;
@@ -35,13 +37,14 @@ public class DiagramaGant extends JXPanel {
      */
     public DiagramaGant() {
         UtilGraphics.setFontCells();
+
         this.controller = new C_Canvas(this);
+        this.cells = new ArrayList<CellGant>();
 
         setBorder(new TitledBorder(null, "Diagrama de Gant", TitledBorder.LEFT, TitledBorder.TOP));
-//        setBackground(Color.BLUE);
-        cells = new ArrayList<CellGant>();
-        dimensionPanel = getSize();
-        counter = new BurstCounter(123);
+
+        this.dimensionPanel = getSize();
+        this.counter = new BurstCounter(123);
 
         addListeners();
 
@@ -51,6 +54,11 @@ public class DiagramaGant extends JXPanel {
         addMouseListener(controller);
     }
 
+    /**
+     * Método que pinta cada celda
+     *
+     * @param g Contexto gráfico
+     */
     public void paintCells(Graphics g) {
         for (CellGant cell : this.cells) {
             cell.paintCell(g);
@@ -66,44 +74,29 @@ public class DiagramaGant extends JXPanel {
         if (processes.size() != 0) {
             this.cells = new ArrayList<CellGant>();
 
-            Dimension cellSize = calculateCellSize(processes.size());
+            // Calcula el tamaño de las celdas
+            Dimension bottomRight = UtilGraphics.calcCellSize(processes.size(), dimensionPanel);
 
-            int xPoint = padds[0];
-            int yPoint = padds[1];
+            int xPoint = UtilGraphics.P_LEFT;
+            int yPoint = UtilGraphics.P_TOP;
+
+            int counter = 0;
 
             for (Process process : processes) {
-                this.cells.add(new CellGant(xPoint, yPoint, cellSize.width, cellSize.height, process.getBurstNum()));
+                this.cells.add(new CellGant(xPoint, yPoint, bottomRight.width, bottomRight.height, process.getBurstNum()));
+                counter += 1;
+
+                xPoint += bottomRight.width;
+
+                if (counter == 10) {
+                    xPoint = UtilGraphics.P_LEFT;
+                    yPoint += UtilGraphics.SPACE_BETWEEN_CELLS_ROW + bottomRight.height;
+
+                    counter = 0;
+                }
             }
         }
 
-    }
-
-    /**
-     * Método que calcula el ancho y alto de las celdas
-     *
-     * @param numProcess numero de procesos
-     * @return punto Right-Bottom de la celda
-     */
-    public Dimension calculateCellSize(int numProcess) {
-        int maxH = 80;
-        int numRows;
-
-        int cellH = maxH;
-        int cellW = (int) Math.floor(dimensionPanel.width / numProcess);
-
-        // Solo si el ancho de la celda es menor al ancho minimo se establece este como valor
-        if (cellW < minSizeCell.width) {
-            cellW = minSizeCell.width;
-        }
-
-        // Si existen mas de 10 procesos se empiezan a crear filas debajo de estos.
-        if (numProcess > minNumCells) {
-            numRows = (int) Math.floor(numProcess / 10);
-            cellH = (int) Math.floor((dimensionPanel.height - spaceBetweenCellsRow * (numRows - 1)) / numRows);
-        }
-
-
-        return new Dimension(cellW, cellH);
     }
 
     /**
@@ -111,14 +104,20 @@ public class DiagramaGant extends JXPanel {
      */
     public void recalculateDimension() {
         dimensionPanel = getSize();
-        dimensionPanel.width = dimensionPanel.width - padds[2] * 2;
-        dimensionPanel.height = dimensionPanel.height - padds[3] * 2;
+        dimensionPanel.width = dimensionPanel.width - UtilGraphics.P_RIGHT * 2;
+        dimensionPanel.height = dimensionPanel.height - UtilGraphics.P_BOTTOM * 2;
     }
 
     public void recalculateComponents(Graphics g) {
         recalculateDimension();
-        Dimension panelBRPoint = new Dimension(dimensionPanel.width + padds[0], dimensionPanel.height + padds[1] * 2 - 9);
+        Dimension panelBRPoint = new Dimension(dimensionPanel.width + UtilGraphics.P_LEFT, dimensionPanel.height + UtilGraphics.P_TOP * 2 - 9);
         counter.calculatePosition(panelBRPoint);
+    }
+
+    public void paintTestGraphics(Graphics g) {
+        String texto = String.format("%d - %d", dimensionPanel.width, dimensionPanel.height);
+        g.drawRect(UtilGraphics.P_LEFT, UtilGraphics.P_TOP, dimensionPanel.width, dimensionPanel.height);
+        g.drawString(texto, UtilGraphics.P_LEFT, UtilGraphics.P_TOP);
     }
 
     /**
@@ -127,19 +126,12 @@ public class DiagramaGant extends JXPanel {
      * @param g contexto de los gráficos
      */
     public void paintComponent(Graphics g) {
-        List<Process> processes = new ArrayList<Process>();
-        processes.add(new Process(1, "P1", 0, 5, 1));
-        processes.add(new Process(2, "P2", 0, 5, 1));
-        processes.add(new Process(3, "P3", 0, 5, 1));
-        processes.add(new Process(4, "P4", 0, 5, 1));
-        processes.add(new Process(5, "P5", 0, 5, 1));
+        List<Process> processes = UtilData.generateProcesses(45);
 
         super.paintComponent(g);
         recalculateComponents(g);
-        String texto = String.format("%d - %d", dimensionPanel.width, dimensionPanel.height);
-        g.drawRect(padds[0], padds[1], dimensionPanel.width, dimensionPanel.height);
 
-        g.drawChars(texto.toCharArray(), 0, texto.length(), 10, 20);
+        paintTestGraphics(g);
 
         processListToCellList(processes);
         paintCells(g);
