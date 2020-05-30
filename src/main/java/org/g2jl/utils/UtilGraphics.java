fe6b703e20cb251f6dfc8ed35c0a1163b9f7c726
Scoowy <gahonajuanjo@gmail.com>
2020-05-29 20:20:48 -0500

    Add alignText function and minor changes

--- src/main/java/org/g2jl/utils/UtilGraphics.java
+++ src/main/java/org/g2jl/utils/UtilGraphics.java
@@ -1,13 +1,12 @@
 package org.g2jl.utils;
 
-import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
 
 /**
  * Clase que contiene métodos y variables estáticas para trabajar con elementos gráficos.
  *
  * @author Juan Gahona
- * @version 20.5.15
+ * @version 20.5.29
  */
 public class UtilGraphics {
     public final static Color STROKE_COLOR = Color.BLACK;
@@ -18,12 +17,16 @@ public class UtilGraphics {
     public final static Font FONT_COUNTER = new Font("Segoe UI", Font.BOLD, 26);
     public static Font fontCells;
 
+    public final static int TEXT_LEFT = 0;
+    public final static int TEXT_RIGHT = 1;
+    public final static int TEXT_CENTER = 2;
+
     private final static double heightFontCell = 0.4;
 
-    public final static int P_LEFT = 20;
-    public final static int P_TOP = 25;
-    public final static int P_RIGHT = 20;
-    public final static int P_BOTTOM = 22;
+    public final static int PADDING_LEFT = 20;
+    public final static int PADDING_TOP = 25;
+    public final static int PADDING_RIGHT = 20;
+    public final static int PADDING_BOTTOM = 22;
 
     // START Celdas
     public final static Dimension MIN_SIZE_CELL = new Dimension(60, 30);
@@ -53,25 +56,10 @@ public class UtilGraphics {
      * Método que calcula el tamaño de la fuente respecto al tamaño de la celda
      *
      * @param cellH height de la celda
-     * @return el tamaño de la fuente
      */
-    public static int calcFontSize(int cellH) {
+    public static void calcFontSize(int cellH) {
         int size = (int) Math.floor(cellH * heightFontCell);
         setFontCells(size);
-        return size;
-    }
-
-    /**
-     * Método que centra un numero vertical y horizontalmente, dentro de un area definida por dos puntos.
-     *
-     * @param g           Contexto gráfico
-     * @param topLeft     Punto inicial del area
-     * @param bottomRight Punto final del area
-     * @param value       Numero a centrar
-     * @return Dimension punto donde dibujarlo
-     */
-    public static Dimension centerText(Graphics g, Dimension topLeft, Dimension bottomRight, int value) {
-        return centerText(g, topLeft, bottomRight, Integer.toString(value));
     }
 
     /**
@@ -80,25 +68,66 @@ public class UtilGraphics {
      * @param g           Contexto gráfico
      * @param topLeft     Punto inicial del area
      * @param bottomRight Punto final del area
-     * @param text        Texto a centrar
-     * @return Dimension punto donde dibujarlo
+     * @param text        Numero a centrar
+     * @param align       alineación respecto de la celda
+     * @return Point punto donde dibujarlo
      */
-    public static Dimension centerText(Graphics g, Dimension topLeft, Dimension bottomRight, String text) {
+    public static Point alignText(Graphics g, Point topLeft, Point bottomRight, int text, int align) {
+        return alignText(g, topLeft, bottomRight, Integer.toString(text), align);
+    }
+
+    /**
+     * Método que alinea un texto vertical y horizontalmente, dentro de un area definida por dos puntos, en relación a una celda de Gant.
+     *
+     * @param g           Contexto gráfico
+     * @param topLeft     Punto inicial del area
+     * @param bottomRight Punto final del area
+     * @param text        Texto a centrar
+     * @param align       alineación respecto de la celda
+     * @return Point punto donde dibujarlo
+     */
+    public static Point alignText(Graphics g, Point topLeft, Point bottomRight, String text, int align) {
         FontMetrics fm = g.getFontMetrics(g.getFont());
         int widthText = fm.stringWidth(text);
         int heightText = fm.getHeight();
 
-        int pX = topLeft.width + ((bottomRight.width - widthText) / 2);
-        int pY = topLeft.height + ((bottomRight.height - heightText) / 2) + fm.getAscent();
+        int pX;
+        int pY;
 
-        return new Dimension(pX, pY);
+        switch (align) {
+            case TEXT_LEFT:
+                pX = topLeft.x + bottomRight.x - widthText;
+                pY = topLeft.y + bottomRight.y + heightText;
+                return new Point(pX, pY);
+
+            case TEXT_RIGHT:
+                pX = topLeft.x - widthText;
+                pY = topLeft.y + bottomRight.y + heightText;
+                return new Point(pX, pY);
+
+            case TEXT_CENTER:
+                pX = topLeft.x + ((bottomRight.x - widthText) / 2);
+                pY = topLeft.y + ((bottomRight.y - heightText) / 2) + fm.getAscent();
+                return new Point(pX, pY);
+
+            default:
+                return new Point(0, 0);
+        }
     }
 
-    public static Dimension calcCellSize(int numProcesses, Dimension panel) {
+    /**
+     * Función que calcula el tamaño óptimo que debe tener las celdas en el diagrama de Gant, teniendo en
+     * cuenta el numero de procesos que se dibujaran.
+     *
+     * @param numProcesses el numero de procesos que existen actualmente
+     * @param panel        la dimension del contenedor
+     * @return Point que determina el tamaño de la celda
+     */
+    public static Point calcCellSize(int numProcesses, Dimension panel) {
         Dimension cellSize = new Dimension(MAX_SIZE_CELL);
         cellSize.width = panel.width / numProcesses;
         System.out.printf("%d - %d\n", cellSize.width, MIN_SIZE_CELL.width);
-        // cellSize.width tendrá el valor minimo de la celda si es inferior a este.
+        // cellSize.width tendrá el valor mínimo de la celda si es inferior a este.
         cellSize.width = Math.max(cellSize.width, MIN_SIZE_CELL.width);
         System.out.printf("%d\n", cellSize.width);
 
@@ -110,6 +139,6 @@ public class UtilGraphics {
             cellSize.height = (panel.height - (SPACE_BETWEEN_CELLS_ROW * numRowsCells)) / numRowsCells;
         }
 
-        return cellSize;
+        return new Point(cellSize.width, cellSize.height);
     }
 }
