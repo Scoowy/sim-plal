3551da726a99189a8bfb0a4cb6f5bdd9c67ddc8f
Scoowy <gahonajuanjo@gmail.com>
2020-05-30 22:20:25 -0500

    Add functions for create fake process

--- src/main/java/org/g2jl/utils/UtilData.java
+++ src/main/java/org/g2jl/utils/UtilData.java
@@ -2,6 +2,7 @@ package org.g2jl.utils;
 
 import java.util.ArrayList;
 
+import org.g2jl.models.M_Main;
 import org.g2jl.models.Process;
 
 /**
@@ -19,18 +20,48 @@ public class UtilData {
 
         for (int i = 1; i <= numProcesses; i++) {
             String name = String.format("%s%d", PROCESS_NAME, i);
-            processes.add(new Process(i, name, 0, 0, 0));
+            processes.add(new Process(i, name, randomBetween(0, 12), randomBetween(1, 8), randomBetween(1, 3)));
         }
 
         return processes;
     }
 
-    public static int StringToInt(String num){
+    /**
+     * Método que carga los valores del ejercicio 1 de l trabajo 3 de Sistemas Operativos
+     *
+     * @param model modelo
+     */
+    public static void ej1Tbj3(M_Main model) {
+        model.addNewProcess("P1", 2, 3, 3);
+        model.addNewProcess("P2", 3, 4, 2);
+        model.addNewProcess("P3", 5, 2, 1);
+        model.addNewProcess("P4", 0, 5, 2);
+        model.addNewProcess("P5", 4, 6, 4);
+    }
+
+    /**
+     * Método que carga N números de procesos con valores aleatorios
+     *
+     * @param model modelo
+     */
+    public static void randomProcess(M_Main model) {
+        ArrayList<Process> processes = generateProcesses(randomBetween(5, 30));
+
+        for (Process process : processes) {
+            model.addNewProcess(process.getName(), process.getArrivalTime(), process.getCpuTime(), process.getPriority());
+        }
+    }
+
+    public static int StringToInt(String num) {
         try {
             return Integer.parseInt(num);
         } catch (NumberFormatException ex) {
             System.err.println(ex.getMessage());
             return -1;
         }
+    }
+
+    public static int randomBetween(int min, int max) {
+        return (int) Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
